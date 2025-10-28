2af83b88c4d08c172e2d4a4994dbd18bdac0cf18
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2025-10-28 15:45:10 +0100

    adding to codebas

--- src/components/todo-list/index.jsx
+++ src/components/todo-list/index.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTodo } from "../../store/slice/todoSlice";
+import { addTodo,deleteTodo, editTodo, fetchTodos } from "../../store/slice/todoSlice";
 
 
 const TodoList = () => {
   const [currentTodo, setCurrentTodo] = useState("");
+  const [editedTodoItem, setEditedTodoItem] = useState(null)
 
 
   const dispatch = useDispatch()
 
-  const {todoList} = useSelector(state => state)
-  // console.log(todoList)
+  const {todoList, todoListFromApi, loading} = useSelector(state => state.todo)
+  console.log(todoListFromApi)
 
 
 
@@ -19,6 +20,30 @@ const TodoList = () => {
     setCurrentTodo('')
   }
 
+  const handleDelete = (getId)=>{
+  dispatch(deleteTodo(getId))
+  }
+
+  const handleEdit = (getCurrentTodo)=>{
+   
+    setEditedTodoItem(getCurrentTodo.id)
+    setCurrentTodo(getCurrentTodo.title)
+  }
+  const handleEditUpdate = ()=>{
+     dispatch(editTodo({
+      currentTodo,
+      editedTodoItem
+    }))
+    setCurrentTodo('')
+  }
+
+  const fetchListOfTodos =()=>{
+    dispatch(fetchTodos())
+  }
+
+  if(loading){
+  return <h1>fetching todo from api</h1>
+  }
 
   return (
     <div>
@@ -27,15 +52,25 @@ const TodoList = () => {
         onChange={(e) => setCurrentTodo(e.target.value)}
         type="text"
         name="todo"
-        placeholder="Enter your todo"
+        placeholder= "Enter your todo"
       />
-      <button disabled={currentTodo == ''} onClick={handleTodo}>Add Todo</button>
+      <button disabled={currentTodo == ''}  onClick={editedTodoItem !== null ? handleEditUpdate : handleTodo}>{editedTodoItem !== null ? `Edit Todo` : `Add Todo`}</button>
 
 
       <ul>
+        {todoList && todoList.length > 0 ? todoList.map(todoItem => <li key={todoItem.id}>
+          <p>{todoItem.title}
+           <button onClick={()=>handleDelete(todoItem.id)}>Delete</button>
+            <button onClick={()=>handleEdit(todoItem)}>Edit</button>
+          </p>
+        
+        </li>  ): null}
+      </ul>
+      <button onClick={fetchListOfTodos}>Fetch list of API</button>
+
+      <ul>
         {
-          todoList && todoList.length > 0 ?
-          todoList.map(todoItem => <li key={todoItem.id}>{todoItem.title}</li>  ): null
+          todoListFromApi && todoListFromApi.length > 0 ? todoListFromApi.map(todoItem => <li key={todoItem.id}>{todoItem.todo}</li>): null
         }
       </ul>
     </div>
--- src/store/slice/todoSlice.js
+++ src/store/slice/todoSlice.js
@@ -1,49 +1,80 @@
-
-//name initialstate reducers
-
-// import { createSlice } from "@reduxjs/toolkit";
-
-//  const initialState ={
-//     todoList:[],
-//  }
-
-// const todoReducer = createSlice({
-//     name:'todos',
-//     initialState: initialState,
-//     reducers:{
-//        //combine all the actions that you need. 
-//        addTodo(state,action){
-//             console.log(action)
-//         return state
-//        }
-//     }
-// })
-
-// export const {addTodo} = todoReducer.actions
-// export default todoReducer.reducer
-
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    todoList:[],
-}
+  todoList: [],
+  loading: false,
+  todoListFromApi : [],
+  isError: false,
+};
 
+export const fetchTodos = createAsyncThunk('fetchTodos', async()=>{
+    const apiResponse = await fetch('https://dummyjson.com/todos')
+    const result = await apiResponse.json()
 
-const todoReducer = createSlice({
-    name:'todos',
-    initialState:initialState,
-    reducers:{
-        addTodo(state,action){
-               const newlyCreatedTodo = {
-                id: Math.random(),
-                title: action.payload,
-               }
-
-               state.todoList.push(newlyCreatedTodo)
-            return state
-        }
-    }
+    return result
 })
 
-export const {addTodo} = todoReducer.actions
-export default todoReducer.reducer
+const todoReducer = createSlice({
+  name: "todos",
+  initialState: initialState,
+  reducers: {
+    //combine all the actions that you need.
+    addTodo(state, action) {
+      const newlyCreatedTodo = {
+        id: state.todoList.length === 0 ? 1 : state.todoList.length + 1,
+        title: action.payload,
+      };
+      state.todoList.push(newlyCreatedTodo);
+      return state;
+    },
+
+    deleteTodo(state, action) {
+      state.todoList = state.todoList.filter(
+        (todoItem) => todoItem.id !== action.payload
+      );
+      return state;
+    },
+
+    editTodo(state, action) {
+      // state.todoList = state.todoList.filter(todoItem => todoItem.id === action.payload)
+      let getTodos = state.todoList;
+
+      let getCurrentTodoIndex = getTodos.findIndex(
+        (item) => item.id === action.payload.currentTodo
+      );
+
+      getTodos[getCurrentTodoIndex] = {
+        ...getTodos[getCurrentTodoIndex],
+        title: action.payload.currentTodo,
+      };
+
+      state.todoList = getTodos;
+      console.log(getCurrentTodoIndex);
+
+      return state;
+    },
+
+   
+  },
+
+  //outside the reducer object
+   extraReducers : (builder)=>{
+        builder.addCase(fetchTodos.pending, (state, action)=>{
+            state.loading = true;
+        })
+
+        builder.addCase(fetchTodos.fulfilled, (state, action)=>{
+            console.log(action.payload.todos[1])
+            state.loading = false;
+            state.todoListFromApi = action.payload.todos;
+        })
+
+        builder.addCase(fetchTodos.rejected, (state, action)=>{
+            state.loading = false;
+            state.isError = true;
+        })
+    }
+});
+
+export const { addTodo, deleteTodo, editTodo } = todoReducer.actions;
+export default todoReducer.reducer;
